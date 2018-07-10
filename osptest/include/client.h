5ab70cf565efb4d845881bb6ae2872f0928a04b9
xiaodezhang <zhangxiaode0@gmail.com>
2018-07-10 16:25:15 +0800

    file upload,compile passed,haven't tested

--- osptest/include/client.h
+++ osptest/include/client.h
@@ -15,6 +15,10 @@
 #define SIGN_STATUS_OUT               0
 #define AUTHORIZATION_NAME_SIZE       20
 
+#define BUFFER_SIZE                   MAX_MSG_LEN
+#define MAX_FILE_NAME_LENGTH         200
+
+
 typedef struct tagSinInfo{
         s8 g_Username[AUTHORIZATION_NAME_SIZE];
         s8 g_Passwd[AUTHORIZATION_NAME_SIZE];
@@ -22,12 +26,25 @@ typedef struct tagSinInfo{
 
 class CCInstance : public CInstance{
 private:
-        u32 m_dwdstNode;
-        u32 wDisInsID;
+        u32         m_dwdstNode;
+        u32         wDisInsID;
+        SEMHANDLE   m_sem;
+        u8          file_name_path[MAX_FILE_NAME_LENGTH];
+
+        u8          buffer[BUFFER_SIZE];
 private:
         void InstanceEntry(CMessage *const);
 public:
-        CCInstance(): m_dwdstNode(0),wDisInsID(0){}
+        CCInstance(): m_dwdstNode(0),wDisInsID(0){
+                OspSemBCreate(&m_sem);
+                memset(file_name_path,0,sizeof(u8)*MAX_FILE_NAME_LENGTH);
+                memset(buffer,0,sizeof(u8)*BUFFER_SIZE);
+        }
+        ~CCInstance(){
+                OspSemDelete(m_sem);
+        }
+        u32 GetDstNode();
+        void FileSendCmd2Client();
 };
 
 typedef zTemplate<CCInstance,MAX_INS_NUM,CAppNoData,MAX_ALIAS_LENGTH> CCApp;
--- osptest/source/client.cpp
+++ osptest/source/client.cpp
@@ -2,6 +2,8 @@
 #include"client.h"
 
 CCApp g_cCApp;
+static u8 CancleFlag = 0;
+
 
 int main(){
 
@@ -31,6 +33,39 @@ int main(){
 
         OspQuit();
         return 0;
+}
+
+
+void test_file_send(){
+
+}
+
+u32 CCInstance::GetDstNode(){
+
+        return m_dwdstNode;
+}
+void CCInstance::FileSendCmd2Client(){
+
+
+#if 0
+    const char file_name_path[] = "test_file_name";
+    FILE *file;
+    size_t buffer_size;
+
+    if(!(file = fopen(file_name_path,"rb"))){
+          printf("open file error\n");
+          return;
+    }
+    buffer_size = fread(buffer,1,sizeof(char)*BUFFER_SIZE,file);
+    post(MAKEIID(SERVER_APP_ID,CInstance::PENDING),FILE_UPLOAD
+                                      ,buffer,buffer_size,GetDstNode());
+
+    while((buffer_size = fread(buffer,1,sizeof(char)*BUFFER_SIZE,file)) > 0){
+          //TODO:Instance实例选择需要修改,增加中断变量
+        post(MAKEIID(SERVER_APP_ID,CInstance::PENDING),FILE_UPLOAD
+                        ,buffer,buffer_size,m_dwdstNode);
+    }
+#endif
 }
 
 void CCInstance::InstanceEntry(CMessage * const pMsg){
@@ -80,7 +115,6 @@ void CCInstance::InstanceEntry(CMessage * const pMsg){
                                           //TODO:向GUI发送重新登陆提示
                                   }
                                   NextState(RUNNING_STATE);
-                                  wDisInsID = pMsg->srcid;
                                   //TODO,应该需要增加其他信息，目前是无法定位的，也有可能log函数自带定位
                                   //，需要确认
                                   OspLog(SYS_LOG_LEVEL,"sign in successfully\n");
@@ -89,10 +123,37 @@ void CCInstance::InstanceEntry(CMessage * const pMsg){
 
                                   //测试登出
                                   //TODO
-                                  //post(MAKEIID(SERVER_APP_ID,CInstance::DAEMON),SIGN_OUT,NULL,0,m_dwdstNode);
-                                  post(wDisInsID,SIGN_OUT,NULL,0,m_dwdstNode);
+                                  post(MAKEIID(SERVER_APP_ID,CInstance::DAEMON),SIGN_OUT,NULL,0,m_dwdstNode);
+//                                  post(wDisInsID,SIGN_OUT,NULL,0,m_dwdstNode);
 					
                                   printf("send sign out\n");
+#if 0
+
+                                  //测试文件上传
+                                  FILE* file;
+                                  size_t buffer_size;
+
+                                  if(!(file = fopen(file_name_path,"rb"))){
+                                          printf("open file error\n");
+                                          break;
+                                  }
+
+                                  buffer_size = fread(buffer,1,sizeof(char)*BUFFER_SIZE,file);
+                                  if(buffer_size > 0){
+                                        post(MAKEIID(SERVER_APP_ID,CInstance::PENDING),FILE_UPLOAD
+                                                   ,buffer,buffer_size,GetDstNode());
+
+                                  }
+                                  //
+                                  while((buffer_size = fread(buffer,1,sizeof(char)*BUFFER_SIZE,file)) > 0){
+                                          //TODO:Instance实例选择需要修改,增加中断变量
+                                        post(MAKEIID(SERVER_APP_ID,CInstance::PENDING),FILE_UPLOAD
+                                                        ,buffer,buffer_size,m_dwdstNode);
+                                  }
+                                  fclose(file);
+#endif
+
+
                              }
                              break;
                      }
@@ -102,7 +163,7 @@ void CCInstance::InstanceEntry(CMessage * const pMsg){
                      switch(curEvent){
                              //TODO：登出命令由GUI发送，根据appid,
                              case SIGN_OUT_CMD:{
-                                  post(wDisInsID,SIGN_OUT,NULL,0,m_dwdstNode);
+                                  post(MAKEIID(SERVER_APP_ID,CInstance::DAEMON),SIGN_OUT,NULL,0,m_dwdstNode);
                                   OspLog(SYS_LOG_LEVEL,"get sign out cmd,send to server\n");
                                   OspPrintf(1,1,"get sign out cmd,send to server\n");
                              }
@@ -112,6 +173,47 @@ void CCInstance::InstanceEntry(CMessage * const pMsg){
                                   OspLog(SYS_LOG_LEVEL,"sign out\n");
                                   OspPrintf(1,1,"sign out\n");
 printf("get sign out ack\n");
+                             }
+                             break;
+                             case FILE_NAME_SEND_CMD:{
+                                  post(MAKEIID(SERVER_APP_ID,CInstance::PENDING),FILE_NAME_SEND
+                                                 ,file_name_path,sizeof(file_name_path),GetDstNode());
+
+                             }
+                             break;
+                             case FILE_NAME_ACK:{
+                                  FILE* file;
+                                  size_t buffer_size;
+
+
+                                  wDisInsID = pMsg->srcid;
+                                  if(!(file = fopen((const char*)file_name_path,"rb"))){
+                                          printf("open file error\n");
+                                          break;
+                                  }
+
+#if 0
+                                  buffer_size = fread(buffer,1,sizeof(char)*BUFFER_SIZE,file);
+                                  if(buffer_size > 0){
+                                        post(MAKEIID(SERVER_APP_ID,CInstance::PENDING),FILE_UPLOAD
+                                                   ,buffer,buffer_size,GetDstNode());
+
+                                  }
+#endif
+
+                                  while((buffer_size = fread(buffer,1,sizeof(char)*BUFFER_SIZE,file)) > 0){
+                                          //TODO:增加中断变量,信号量控制,记录文件读取位置
+                                          //大文件传送测试
+                                          post(wDisInsID,FILE_UPLOAD,buffer,buffer_size,m_dwdstNode);
+                                          OspSemTake(m_sem);
+                                          if(CancleFlag){
+                                                  OspSemGive(m_sem);
+                                                  break;
+                                          }
+                                          OspSemGive(m_sem);
+                                  }
+                                  fclose(file);
+
                              }
                              break;
                      }
